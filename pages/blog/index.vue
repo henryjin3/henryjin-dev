9d710ebec27445be170dd5daf29bb6be6254d9f6
Henry <henry@thejins.org>
2020-04-07 14:56:22 -0500

    Add methods to pull CMS data

---
+++ pages/blog/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    blog index
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts;
+    }
+  }
+};
+</script>
