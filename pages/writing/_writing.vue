94b11f00c8c9e061050c726a564bf672d8a852a0
Henry <henry@thejins.org>
2020-04-08 10:39:23 -0500

    add extremely basic writing list, clean up blog refs

---
+++ pages/writing/_writing.vue
@@ -0,0 +1,18 @@
+<template>
+  <article>
+    <h1>{{ title }}</h1>
+    <!-- eslint-disable-next-line -->
+    <div class="content" v-html="post" />
+  </article>
+</template>
+<script>
+export default {
+  async asyncData({ params }) {
+    const markdown = await require(`~/assets/content/writing/${params.writing}/${params.writing}.md`);
+    return {
+      post: markdown.html,
+      title: markdown.attributes.title
+    };
+  }
+};
+</script>
--- pages/writing/index.vue
+++ pages/writing/index.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    blog index
+    writing index
+    <section>
+      <ol>
+        <li v-for="post in posts" :key="post.attributes.title">
+          <nuxt-link :to="post.path">
+            {{ post.attributes.title }}
+          </nuxt-link>
+        </li>
+      </ol>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    blogPosts() {
-      return this.$store.state.blogPosts;
-    }
+  async asyncData() {
+    const context = await require.context(
+      '~/assets/content/writing',
+      true,
+      /\.md$/
+    );
+    const posts = await context.keys().map((key) => {
+      const pathStr = key.replace('.md', '').replace('./', '');
+      return {
+        ...context(key),
+        path: `/writing/${pathStr.substring(0, pathStr.lastIndexOf('/'))}`
+      };
+    });
+    return { posts: posts.reverse() };
   }
 };
 </script>
