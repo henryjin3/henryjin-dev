89f495bc5086b646f9e5f93e4c10825b3bb9f6d9
Henry <henry@thejins.org>
2020-04-13 11:34:00 -0500

    remove last netlify identity instance

--- pages/index.vue
+++ pages/index.vue
@@ -11,15 +11,7 @@
 </template>
 
 <script>
-export default {
-  head() {
-    return {
-      script: [
-        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
-      ]
-    };
-  }
-};
+export default {};
 </script>
 
 <style scoped>
