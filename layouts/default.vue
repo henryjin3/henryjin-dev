739d76b02f801d95ad1c23677a8226c6bac72a9d
Henry <henry@thejins.org>
2020-04-20 14:27:33 -0500

    add components folder, clear out readmes

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script>
-import Header from './header';
-import Footer from './footer';
+import Header from '../components/header';
+import Footer from '../components/footer';
 
 export default {
   components: {
